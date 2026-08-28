a4994665541b01248d0c9fdb01bd643319317b5b
dattmistry2026 <dattmistry2026@gmail.com>
2026-08-28 10:45:27 +1000

    code update

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div>
         {user ? (
           <>
-            <Link to="/rooms" className="mr-4">Tasks</Link>
+            <Link to="/rooms" className="mr-4">Rooms</Link>
             <Link to="/profile" className="mr-4">Profile</Link>
             <button
               onClick={handleLogout}
